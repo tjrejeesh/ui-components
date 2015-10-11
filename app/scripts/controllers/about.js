05642415c391299d24460d71da148683b2ed8b4c
rejeesh <rejeeshtj1986@gmail.com>
2015-10-11 17:54:16 +0530

    App Setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reusableApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the reusableApp
+ */
+angular.module('reusableApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
